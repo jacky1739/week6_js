69dbe03299bcb8234391d859ab703433bdda7aa5
jacky123789456@gmail.com <jacky123789456@gmail.com>
2021-03-30 01:15:35 +0800

    prototype 1 completed

--- all.js
+++ all.js
@@ -91,5 +91,63 @@ function addData(){
     addStr();
 }
 
+regionSearch.addEventListener("change", function(){
+    // console.log(regionSearch.value);
+    render(regionSearch.value);
+})
+
+function render(location) {
+    let str = "";
+    let newData = data.filter(function(item){
+        if(location === item.area){
+            return item
+        }
+        if(!location){
+            return item
+        }
+    })
+    console.log(newData);
+    
+
+    let searchResultNum = `本次搜尋共有${newData.length}次`;
+    console.log(newData.length);
+
+
+    newData.forEach(function(item){
+        str += `<li class="ticketCard">
+                    <div class="ticketCard-img">
+                        <a href="#">
+                            <img src="${item.imgUrl}" alt="">
+                        </a>
+                        <div class="ticketCard-region">${item.area}</div>
+                        <div class="ticketCard-rank">${item.rate}</div>
+                    </div>
+                    <div class="ticketCard-content">
+                        <div>
+                            <h3>
+                                <a href="#" class="ticketCard-name">${item.name}</a>
+                            </h3>
+                            <p class="ticketCard-description">
+                            ${item.description}
+                            </p>
+                        </div>
+                        <div class="ticketCard-info">
+                            <p class="ticketCard-num">
+                                <span><i class="fas fa-exclamation-circle"></i></span>
+                                剩下最後 <span id="ticketCard-num"> ${item.group} </span> 組
+                                </p>
+                                <p class="ticketCard-price">
+                                TWD <span id="ticketCard-price">${item.price}</span>
+                            </p>
+                        </div>
+                    </div>
+            </li>`;
+    });
+
+    searchResult.innerHTML = searchResultNum;
+    list.innerHTML = str;
+
+}
+
 
 init();
